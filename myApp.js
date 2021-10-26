9186fdbb627c0d2c5ea7d719693fcd94728cd677
rabieOuallaf1 <targaryenrabie@gmail.com>
2021-10-26 16:26:33 +0200

    add a json

--- myApp.js
+++ myApp.js
@@ -1,23 +1,20 @@
 var express = require('express');
 var app = express();
-var absolutePath = __dirname + /views/index.html;
-app.get("/", function(req, res){ 
+var absolutePath = __dirname + "/views/index.html";
+var staticPath = __dirname + "/public ";
+
+
+app.get("/", function(req, res, next){ 
     res.sendFile(absolutePath);
+    next;
 });
 
-    
+app.use('/public', express.static(staticPath));    
 
 
-
-
-
-
-
-
-
-
-
-
+app.get("/json", function(req, res){
+    res.json({"message": "Hello json"});
+});
 
 
 
