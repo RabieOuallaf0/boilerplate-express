0940e2d9c667a86d068ae26aa6a01b78cdb9c1ea
rabieOuallaf1 <targaryenrabie@gmail.com>
2021-10-29 16:46:08 +0200

    edit myApp.js

--- myApp.js
+++ myApp.js
@@ -14,11 +14,12 @@ app.use('/public', express.static(staticPath));
 
 
 app.get("/json", function(req, res) {
-    if(process.env.MESSAGE_STYLE === "uppercase") {
-        res.json({"message" : "HELLO JSON"});
-    }else {
-        res.json({"message" : "hello json"});
+    var jsonRes = {"message" : "hello world"};
+    if(process.env.MESSAGE_STYLE = "uppercase"){
+        jsonRes.message = jsonRes.message.toUpperCase()
     };
+
+    res.json(jsonRes);
 });
 
 
