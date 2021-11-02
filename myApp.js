e7b863402909d41fe4cf72aa233b96b2fe5b7986
rabieOuallaf1 <targaryenrabie@gmail.com>
2021-11-02 14:46:13 +0100

    New modifications

--- myApp.js
+++ myApp.js
@@ -15,9 +15,7 @@ app.use('/public', express.static(staticPath));
 app.get("/json", function(req, res){
     if (process.env.MESSAGE_STYLE === "uppercase") {
         res.json({"message": "HELLO JSON"});
-    }else {
-        res.json({"message": "hello json"});
-    };
+    }
 });
 
 
