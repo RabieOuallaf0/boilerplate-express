1cd9d539128bc1d17a7a20a9fb6d355e0deb98ba
rabieOuallaf1 <targaryenrabie@gmail.com>
2021-11-03 19:26:39 +0100

    fixing bugs

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 var absolutePath = __dirname + "/views/index.html";
 var staticPath = __dirname + "/public ";
-require('dotenv').config();
 
 
 app.get("/", function(req, res){ 
@@ -15,7 +15,9 @@ app.use('/public', express.static(staticPath));
 app.get("/json", function(req, res){
     if (process.env.MESSAGE_STYLE === "uppercase") {
         res.json({"message": "HELLO JSON"});
-    }
+    }else {
+        res.json({'message': 'hello json'});
+    };
 });
 
 
